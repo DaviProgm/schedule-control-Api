0f5882e3ea2e58debdf527f91dadae769ce57564
Davi Monteiro <d8119514@gmail.com>
2025-06-18 23:16:08 -0300

    Create a Schedule  System!

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 4005;
+const cors = require('cors');
+
 
 const { User } = require('./src/models');
 const UserRouter = require('./src/routes/users');
 const AuthRouter = require('./src/routes/auth')
 const ScheduleRouter = require("./src/routes/schedule")
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/users', UserRouter);
 app.use('/auth', AuthRouter)
 app.use(ScheduleRouter)
