4367ced2c65fe387c25678a59d2f894ddcf26dd3
Davi Monteiro <d8119514@gmail.com>
2025-06-18 23:10:29 -0300

    Create a Schedule  System!

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -1,32 +1,31 @@
-const sequelize = require('../config/database');
 const { DataTypes } = require('sequelize');
-
+const sequelize = require('../config/database');
 
 const Schedule = sequelize.define('Schedule', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    service: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    date: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-    },
-    time: {
-        type: DataTypes.TIME,
-        allowNull: false,
-    },
-    observations: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-},
-    {
-        tableName: 'schedule',
-        freezeTableName: true
-    }
-)
-    module.exports = {Schedule}
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  service: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  time: {
+    type: DataTypes.STRING, 
+    allowNull: false,
+  },
+  observations: {
+    type: DataTypes.TEXT, 
+    allowNull: true, 
+  },
+}, {
+  tableName: 'schedules',
+  freezeTableName: true,
+  timestamps: true,
+});
+
+module.exports = Schedule;
