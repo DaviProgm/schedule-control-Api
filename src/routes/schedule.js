cec2ee3fcd488c690004cae2053ab474a5e19066
Davi Monteiro <d8119514@gmail.com>
2025-06-22 19:10:09 -0300

    filtrando Agendamentos

--- src/routes/schedule.js
+++ src/routes/schedule.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-const ScheduleController = require("../controllers/schedule")
-const ScheduleMiddleware = require("../middleware/schedule")
+const ScheduleController = require("../controllers/schedule");
+const ScheduleMiddleware = require("../middleware/schedule");
 const authMiddleware = require('../middleware/auth');
-const DeleteSchedule = require("../middleware/schedule")
-const UpdateScheduleStatus = require("../controllers/schedule")
-router.use(authMiddleware); 
 
-router.post('/agendamentos',
+router.use(authMiddleware);
+
+router.post(
+  '/agendamentos',
   ScheduleMiddleware.ValidadeCreateSchedule,
   ScheduleController.CreateSchedule
 );
 
-router.get('/agendamentos', ScheduleController.GetSchedules)
-router.put("/agendamentos/:id", ScheduleMiddleware.ValidadeCreateSchedule,ScheduleController.UpdateSchedule);
-router.delete('/:id', ScheduleController.DeleteSchedules);
-router.put("/:id/status", ScheduleController.UpdateScheduleStatus );
+router.get('/agendamentos', (req, res) => {
+  if (req.query.clientId) {
+    return ScheduleController.GetSchedulesByClient(req, res);
+  }
+  return ScheduleController.GetSchedules(req, res);
+});
+router.get('/agendamentos', (req, res) => {
+  if (req.query.clientId) {
+    return ScheduleController.GetSchedulesByClient(req, res);
+  }
+  return ScheduleController.GetSchedules(req, res);
+});
+
+router.put(
+  "/agendamentos/:id",
+  ScheduleMiddleware.ValidadeCreateSchedule,
+  ScheduleController.UpdateSchedule
+);
+
+router.delete('/agendamentos/:id', ScheduleController.DeleteSchedules);
+
+router.put('/agendamentos/:id/status', ScheduleController.UpdateScheduleStatus);
+
 module.exports = router;
