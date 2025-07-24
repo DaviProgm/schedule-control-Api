e45bca29d9d1d34c8636bb4ebb7070265e53dea7
Davi Monteiro <d8119514@gmail.com>
2025-07-23 23:46:18 -0300

    adicionando role ao controllers/user.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,24 +1,30 @@
 require("dotenv").config();
 const bcrypt = require('bcrypt');
 
-const {User} = require('../models/users')
+const { User } = require('../models/users');
 
-async function CreateUser(req,res){
+async function CreateUser(req, res) {
     try {
-        const {name, email, password} = req.body;
-        const hashPassword = await bcrypt.hash(password,10);
-        const user = await User.create({name, email, password: hashPassword});
+        const { name, email, password, role } = req.body;
 
+        const validRoles = ['customer', 'creditor'];
+        if (!validRoles.includes(role)) {
+            return res.status(400).send({ message: "Role inválido. Use: customer, creditor ou admin" });
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ name, email, password: hashPassword, role });
 
         return res.status(201).send(user);
     } catch (error) {
-        console.error('Erro ao criar usuario', error)
+        console.error('Erro ao criar usuário', error);
         return res.status(500).send({
-            message:"Erro ao criar usuario",
-            error:error.message
-        })
+            message: "Erro ao criar usuário",
+            error: error.message
+        });
     }
 }
-module.exports={
+
+module.exports = {
     CreateUser
-}
+};
