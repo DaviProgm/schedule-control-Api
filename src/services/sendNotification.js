0c4d4e4e994236ee224f27fb8796849ee070861c
Davi Monteiro <d8119514@gmail.com>
2025-08-04 16:09:44 -0300

    Migrando para Cron

--- src/services/sendNotification.js
+++ src/services/sendNotification.js
@@ -1,14 +1,4 @@
-const admin = require("firebase-admin");
-const path = require("path");
-
-const serviceAccount = require(path.resolve(__dirname, "../../cloktrix-firebase-adminsdk-fbsvc-8b8a09a465.json"));
-
-// Verifica se já existe uma instância do app
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+const admin = require("../config/firebaseAdmin");
 
 const sendPushNotification = async (fcmToken, notification) => {
   const message = {
