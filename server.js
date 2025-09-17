a594cf66bb0433bb6cb66498ff5fb942ed7e1087
Davi Monteiro <d8119514@gmail.com>
2025-09-17 20:56:59 -0300

    subindo bibles

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   console.log(`REQ: ${req.method} ${req.url}`);
   next();
 });
-
+//
 app.use('/users', UserRouter);
 app.use('/auth', AuthRouter)
 app.use('/agendamentos', authMiddleware, checkActiveSubscription, scheduleRoutes);
