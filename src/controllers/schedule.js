04358eced0e91dffbfd02149de5f708688ecb8dd
Davi Monteiro <d8119514@gmail.com>
2025-09-19 23:45:17 -0300

    notifica cliente via gmail v2

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -36,7 +36,7 @@ async function CreateSchedule(req, res) {
                         <td align="center" style="padding: 20px 0;">
                             <table width="600" border="0" cellspacing="0" cellpadding="0" style="background-color: #ffffff; border-radius: 8px; box-shadow: 0 4px 8px rgba(0,0,0,0.1); overflow: hidden;">
                                 <tr>
-                                    <td align="center" style="padding: 30px 20px; background-color: #6b0082; color: #ffffff;">
+                                    <td align="center" style="padding: 30px 20px; background-color: #0000; color: #ffffff;">
                                         <img src="https://i.ibb.co/svh74hCH/Whats-App-Image-2025-09-18-at-01-23-19-removebg-preview.png" alt="Workgate Logo" width="180" style="display: block; margin-bottom: 20px;"/>
                                         <h1 style="margin: 0; font-size: 24px; color: #ffffff;">Agendamento Confirmado!</h1>
                                     </td>
